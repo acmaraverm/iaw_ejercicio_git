feec5c3615068a06e34e89016421d4ae261e451a
AntonioCarlosMaraver <acmaraverm@gmail.com>
2021-10-17 16:48:25 +0200

    Segundo commit del archivo holamundo.php

--- holamundo.php
+++ holamundo.php
@@ -1,1 +1,10 @@
-<?php echo 'Hola mundo'; ?>
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Prueba de Git - Segundo commit</title>
+</head>
+<body>
+	<?php echo 'Hola mundo'; ?>
+</body>
+</html>
