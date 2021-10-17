38f7b144b3e81965bf6afc1bab554fd22b3604a1
AntonioCarlosMaraver <acmaraverm@gmail.com>
2021-10-17 16:54:12 +0200

    Tercer commit del archivo holamundo.php con html y css

--- holamundo.php
+++ holamundo.php
@@ -2,9 +2,10 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="holamundo.css">
 	<title>Prueba de Git - Segundo commit</title>
 </head>
 <body>
-	<?php echo 'Hola mundo'; ?>
+	<h1><?php echo 'Hola mundo'; ?></h1>
 </body>
 </html>
